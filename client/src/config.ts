ace69131f3bda5dff4b5965274c70a1aaf2a3fb4
Udita Podder <uditapodder22@gmail.com>
2020-06-11 00:58:28 +0530

    initial commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'a1bo9gl6u2'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-udita.auth0.com',
+  clientId: 'dF0PDSesffdrJ48uMHBIBU5nQplJYR9j',
+  callbackUrl: 'http://localhost:3000/callback'
+}
